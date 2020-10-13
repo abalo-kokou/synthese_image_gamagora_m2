deab5c035854e524db160957724d8036b78a9d22
abalo-kokou <kokouabalo@gmail.com>
2020-10-13 13:40:28 +0200

    first commit- bone animation

---
+++ LineRenderer/Assets/LineRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRenderer : MonoBehaviour
+{
+    public LineRenderer lineRenderer; //This will be available in the inspector
+    float length;
+    Vector3 startPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = gameObject.GetComponent<LineRenderer>();
+        //startPoint = lineRenderer.GetPosition(0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
