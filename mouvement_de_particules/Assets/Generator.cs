47d258d8cbab9759b8390644eac9279a2866553e
abalo-kokou <kokouabalo@gmail.com>
2020-10-20 16:59:06 +0200

    mouvement de particules

---
+++ mouvement_de_particules/Assets/Generator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public GameObject cube;
+    public int min, max, numberOfCubes;
+    public Vector3 gravity, velocity;
+    protected List<GameObject> listeOfCubes = new List<GameObject>();
+    int x, y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlaceCubes();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var cube in listeOfCubes)
+        {
+            x = Random.Range(-10, 10);
+            y = Random.Range(-10, 10);
+            velocity = new Vector3(x, y, 0);
+            cube.transform.position += velocity * Time.deltaTime;
+
+            //if (cube.transform.position.y < 0) cube.transform.position
+        }
+    }
+
+    Vector3 GeneratedPosition()
+    {
+        int x, y, z;
+        x = Random.Range(min, max);
+        y = Random.Range(min, max);
+        z = Random.Range(min, max);
+        return new Vector3(x, y, z);
+    }
+    void PlaceCubes()
+    {
+        for (int i = 0; i < numberOfCubes; i++)
+        {
+            // Instantiate(cube, GeneratedPosition(), Quaternion.identity);
+            listeOfCubes.Add(Instantiate(cube, GeneratedPosition(), Quaternion.identity));
+        }
+    }
+}
+
+
---
+++ mouvement_de_particules/Assets/Particules.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particules : MonoBehaviour
+{
+    public Vector3 gravity, velocity;
+    float FORCE_OF_GRAVITY = 9.8F;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gravity = new Vector3(0, FORCE_OF_GRAVITY*(-1), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += gravity*Time.deltaTime;
+    }
+}
