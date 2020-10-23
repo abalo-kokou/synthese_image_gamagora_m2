0a134c5a30f953579de971fb7498c63e83c04de1
abalo-kokou <kokouabalo@gmail.com>
2020-10-23 16:14:13 +0200

    Mouvement de particules -- suite

--- mouvement_de_particules/Assets/Generator.cs
+++ mouvement_de_particules/Assets/Generator.cs
@@ -7,27 +7,40 @@ public class Generator : MonoBehaviour
     public GameObject cube;
     public int min, max, numberOfCubes;
     public Vector3 gravity, velocity;
-    protected List<GameObject> listeOfCubes = new List<GameObject>();
+    [Range(0f, 2f)]
+    public float h = 1f;
+    //protected List<GameObject> listeOfCubes = new List<GameObject>();
+    private GameObject[] tabOfCubes;
     int x, y;
+    //public Vector3 gravity = new Vector3(0, -9.8f, 0);
 
     // Start is called before the first frame update
     void Start()
     {
         PlaceCubes();
-        
+        gravity = new Vector3(0, -9.8f, 0);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        foreach (var cube in listeOfCubes)
+        //foreach (var cube in listeOfCubes)
+        //{
+        //    x = Random.Range(-10, 10);
+        //    y = Random.Range(-10, 10);
+        //    velocity = new Vector3(x, y, 0);
+        //    cube.transform.position += velocity * Time.deltaTime;
+        //    if (cube.transform.position.y < 0) 
+        //    { cube.transform.position = new Vector3(0, 0, 0); }
+        //}
+
+        foreach (GameObject c in tabOfCubes)
         {
-            x = Random.Range(-10, 10);
-            y = Random.Range(-10, 10);
-            velocity = new Vector3(x, y, 0);
-            cube.transform.position += velocity * Time.deltaTime;
-            if (cube.transform.position.y < 0) 
-            { cube.transform.position = new Vector3(0, 0, 0); }
+            Particules cObject = c.GetComponent<Particules>();
+            cObject.speed += Time.deltaTime * gravity;
+            cObject.CheckNeighbors();
+            Debug.Log("cObject.speed " + cObject.speed);
         }
     }
 
@@ -41,10 +54,12 @@ public class Generator : MonoBehaviour
     }
     void PlaceCubes()
     {
+        tabOfCubes = new GameObject[numberOfCubes];
         for (int i = 0; i < numberOfCubes; i++)
         {
             // Instantiate(cube, GeneratedPosition(), Quaternion.identity);
-            listeOfCubes.Add(Instantiate(cube, GeneratedPosition(), Quaternion.identity));
+            tabOfCubes[i] = Instantiate(cube, GeneratedPosition(), Quaternion.identity);
+            tabOfCubes[i].GetComponent<Particules>().h = h;
         }
     }
 }
--- mouvement_de_particules/Assets/Particules.cs
+++ mouvement_de_particules/Assets/Particules.cs
@@ -4,18 +4,45 @@ using UnityEngine;
 
 public class Particules : MonoBehaviour
 {
-    public Vector3 gravity, velocity;
+    //public Vector3 gravity, velocity;
+    //public Vector3 gravity, velocity;
     float FORCE_OF_GRAVITY = 9.8F;
+    public Collider[] neighbors;
+    public float h;
+    public Vector3 speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        gravity = new Vector3(0, FORCE_OF_GRAVITY*(-1), 0);
+       // gravity = new Vector3(0, FORCE_OF_GRAVITY*(-1), 0);
+    }
+
+    public void Border()
+    {
+        if (transform.position.y <= -5)
+            transform.position = new Vector3(transform.position.x, -4.9f, transform.position.z);
+        if (transform.position.y >= 5)
+            transform.position = new Vector3(transform.position.x, 4.9f, transform.position.z);
+
+        if (transform.position.x <= -10)
+            transform.position = new Vector3(-9.9f, transform.position.y, transform.position.z);
+        if (transform.position.x >= 10)
+            transform.position = new Vector3(9.9f, transform.position.y, transform.position.z);
+
+        if (transform.position.z <= -4)
+            transform.position = new Vector3(transform.position.x, transform.position.y, -3.9f);
+        if (transform.position.z >= 4)
+            transform.position = new Vector3(transform.position.x, transform.position.y, 3.9f);
+    }
+    public void CheckNeighbors()
+    {
+        neighbors = Physics.OverlapSphere(transform.position, h);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += gravity*Time.deltaTime;
+        transform.position += speed;
+        Border();
     }
 }
